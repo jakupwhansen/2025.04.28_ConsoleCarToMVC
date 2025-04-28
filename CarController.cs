999a17c6bef09f89cfdbe7893085ed8f144a4874
jakupwhansen <jakupwhansen@hotmail.com>
2025-04-28 14:04:33 +0200

    MVC 03 (Vi har nu oprettet en CarController, som styrer hele programmets logik. Controlleren håndterer brugerens valg, og samarbejder med både View og Model. Program-klassen starter kun Controlleren og har ikke længere logik selv. Dette adskiller ansvar i koden, så hver del kun gør én ting, som i rigtig MVC. Controlleren kalder View for brugerinput og output, og opdaterer Model baseret på valgene.)

---
+++ CarController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleCarToMVC
+{
+    // CarController styrer programmets logik.
+    // Den håndterer brugerens valg og styrer samarbejdet mellem View og Model.
+
+    class CarController
+    {
+        private List<Car> cars = new List<Car>();  // Liste til at gemme bilerne
+        private ICarView view;                      // Bruger interface så vi kan skifte View fleksibelt
+
+        public CarController()
+        {
+            // Her vælger vi hvilket View vi vil bruge.
+            // view = new CarView();
+            view = new ColorCarView();
+
+            // Tilføjer to standardbiler så der er noget data fra starten
+            cars.Add(new Car("Toyota", "Corolla", 2020));
+            cars.Add(new Car("Ford", "Mustang", 1967));
+        }
+
+        // Run-metoden styrer hovedflowet i applikationen
+        public void Run()
+        {
+            bool running = true;
+
+            while (running)
+            {
+                view.ShowMenu();
+                string choice = view.ReadChoice();
+
+                switch (choice)
+                {
+                    case "1":
+                        AddCar();
+                        break;
+                    case "2":
+                        ShowCars();
+                        break;
+                    case "3":
+                        running = false;
+                        break;
+                    default:
+                        view.ShowInvalidChoice();
+                        break;
+                }
+            }
+        }
+
+        // Tilføjer en ny bil baseret på brugerinput
+        private void AddCar()
+        {
+            string brand = view.ReadBrand();
+            string model = view.ReadModel();
+            int year = view.ReadYear();
+
+            Car car = new Car(brand, model, year);
+            cars.Add(car);
+
+            view.ShowCarAdded();
+        }
+
+        // Viser alle biler til brugeren
+        private void ShowCars()
+        {
+            if (cars.Count == 0)
+            {
+                view.ShowNoCars();
+            }
+            else
+            {
+                view.ShowCars(cars);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,85 +1,16 @@
-﻿using ConsoleCarToMVC;
-using System;
-using System.Collections.Generic;
-using System.Runtime.ConstrainedExecution;
+﻿using System;
+using ConsoleCarToMVC;
 
-// Første skridt mod MVC: 
-// Vi flytter al brugerinput og output til en separat View-klasse.
-// Program-klassen holder nu mest styr på logikken (controller-rollen).
-
-class Program
+namespace ConsoleCarToMVC
 {
-
-    private List<Car> cars = new List<Car>();
-    private ICarView view;
-
-    // Constructor
-    public Program()
+    // Program-klassen er nu kun ansvarlig for at starte programmet.
+    // Den skaber en Controller og kalder dens Run()-metode.
+    class Program
     {
-        // Her vælger vi konkret View (Console-view)
-        //view = new CarView();
-        view = new ColorCarView();
-
-        // Vi tilføjer 2 standardbiler, så der er noget at vise
-        cars.Add(new Car("Toyota", "Corolla", 2020));
-        cars.Add(new Car("Ford", "Mustang", 1967));
-    }
-
-    static void Main(string[] args)
-    {
-        Program program = new Program();
-        program.Run();
-    }
-
-    public void Run()
-    {
-        bool running = true;
-
-        while (running)
+        static void Main(string[] args)
         {
-            view.ShowMenu();
-            string choice = view.ReadChoice();
-
-            switch (choice)
-            {
-                case "1":
-                    AddCar();
-                    break;
-                case "2":
-                    ShowCars();
-                    break;
-                case "3":
-                    running = false;
-                    break;
-                default:
-                    view.ShowInvalidChoice();
-                    break;
-            }
-        }
-    }
-
-    private void AddCar()
-    {
-        string brand = view.ReadBrand();
-        string model = view.ReadModel();
-        int year = view.ReadYear();
-
-        Car car = new Car(brand, model, year);
-        cars.Add(car);
-
-        view.ShowCarAdded();
-    }
-
-    private void ShowCars()
-    {
-        if (cars.Count == 0)
-        {
-            view.ShowNoCars();
-        }
-        else
-        {
-            view.ShowCars(cars);
+            CarController controller = new CarController();
+            controller.Run();
         }
     }
 }
-
